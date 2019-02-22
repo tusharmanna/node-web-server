41ac16e2d7d723515e598e395a954f965e05072a
tusharmanna <47896755+tusharmanna@users.noreply.github.com>
2019-02-22 11:39:10 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use( (req,res,next) =>{
+  var now = new Date().toDateString();
+  console.log(`${now}:${req.method}  ${req.url}`);
+  next();
+}
+
+);
+
+hbs.registerHelper('getCurrentyear', ()=> {return  new Date().getFullYear()} );
+
+hbs.registerHelper('screamIT', (text)=> {
+  return  text.toUpperCase();
+} );
+
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+   
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+
+    
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(5000, () => {
+  console.log('Server is up on port 5000');
+});
