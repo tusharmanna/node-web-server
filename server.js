af5842c693ddd1d892f7e8c704aa5e7952fbc795
tusharmanna <47896755+tusharmanna@users.noreply.github.com>
2019-02-22 14:49:57 -0500

    setuo start script herokuy port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000 ;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -45,6 +47,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log('Server is up on port 5000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
